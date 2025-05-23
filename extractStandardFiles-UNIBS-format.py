b05209e5f1f47939ccbb00e45ce530b6d8e72e37
MIRANDA CAVALCANTE NETO Luiz <luiz.miranda@imt-nord-europe.fr>
2025-05-23 11:33:51 +0200

    back to normal

--- extractStandardFiles-UNIBS-format.py
+++ extractStandardFiles-UNIBS-format.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
-import argparse
-import os
 
 def process_file(input_path, output_dir):
     print(f"procesing...")
+    
+def main():
+    process_file("a","b")
+
+if __name__ == "__main__":
+    main()
